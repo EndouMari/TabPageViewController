8ff7b8e02a9b759ef8b84ffbe2122249efb2d919
M"e <endo@vasily.jp>
2016-08-06 17:06:55 +0900

    fixed carthage

---
+++ TabPageViewController/TabPageViewController.h
@@ -0,0 +1,19 @@
+//
+//  TabPageViewController.h
+//  TabPageViewController
+//
+//  Created by EndouMari on 2016/08/06.
+//  Copyright © 2016年 EndouMari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TabPageViewController.
+FOUNDATION_EXPORT double TabPageViewControllerVersionNumber;
+
+//! Project version string for TabPageViewController.
+FOUNDATION_EXPORT const unsigned char TabPageViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TabPageViewController/PublicHeader.h>
+
+
