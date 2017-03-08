7d91a0d64b76576628c098414a5f7d8bd095dbeb
M"e <endo@vasily.jp>
2017-03-08 16:24:16 +0900

    Fixed tabBackgroundColor

--- Sources/TabPageOption.swift
+++ Sources/TabPageOption.swift
@@ -15,13 +15,17 @@ public struct TabPageOption {
     public var fontSize = UIFont.systemFontSize
     public var currentColor = UIColor(red: 105/255, green: 182/255, blue: 245/255, alpha: 1.0)
     public var defaultColor = UIColor(red: 153/255, green: 153/255, blue: 153/255, alpha: 1.0)
-    public var tabBarAlpha: CGFloat = 0.95
     public var tabHeight: CGFloat = 32.0
     public var tabMargin: CGFloat = 20.0
     public var tabWidth: CGFloat?
-    public var tabBackgroundColor: UIColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.95)
+    public var tabBackgroundColor: UIColor = .white
     public var pageBackgoundColor: UIColor = UIColor.white
-     internal var tabBackgroundImage: UIImage {
+    public var isTranslucent: Bool = true
+
+    internal var tabBarAlpha: CGFloat {
+        return isTranslucent ? 0.95 : 1.0
+    }
+    internal var tabBackgroundImage: UIImage {
         return convertImage()
     }
 
@@ -29,7 +33,8 @@ public struct TabPageOption {
         let rect : CGRect = CGRect(x: 0, y: 0, width: 1, height: 1)
         UIGraphicsBeginImageContext(rect.size)
         let context : CGContext? = UIGraphicsGetCurrentContext()
-        context?.setFillColor(tabBackgroundColor.cgColor)
+        let backgroundColor = tabBackgroundColor.withAlphaComponent(tabBarAlpha).cgColor
+        context?.setFillColor(backgroundColor)
         context?.fill(rect)
         let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
         UIGraphicsEndImageContext()
